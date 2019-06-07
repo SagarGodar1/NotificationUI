4cacdac3e5fe69a753f3b65427eedcf8e862c22d
Softwarica College Lab <softwarica@softwarica.com>
2019-06-07 08:16:41 +0545

    create channel and display messages

--- app/src/main/java/com/example/notificationui/MainActivity.java
+++ app/src/main/java/com/example/notificationui/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.notificationui;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import createchannel.CreateChannel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnDisplayNotification = findViewById(R.id.btnDisplayNotification);
+
+
         btnDisplayNotification2 = findViewById(R.id.btnDisplayNotification2);
         notificationManagerCompat = NotificationManagerCompat.from(this);
+        CreateChannel channel = new CreateChannel(this);
+        channel.createChannel();
+
         
         btnDisplayNotification.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +46,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void DisplayNotification2() {
+        Notification notification = new NotificationCompat.Builder(this,CreateChannel.CHANNEL_2)
+                .setSmallIcon(R.drawable.ic_sms_notification)
+                .setContentText("Second message body")
+                .setContentTitle("Second Message")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManagerCompat.notify(2,notification);
     }
 
     private void DisplayNotification() {
+        Notification notification = new NotificationCompat.Builder(this,CreateChannel.CHANNEL_1)
+                .setSmallIcon(R.drawable.ic_sms_notification)
+                .setContentTitle("First Message")
+                .setContentText("First message body")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManagerCompat.notify(1,notification);
+
+
+
     }
 }
